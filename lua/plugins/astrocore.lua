7bb073b5336e318adf6436656e0bc61812af5ddd
doraemon-hub-art <1660219734@qq.com>
2026-03-11 11:08:21 +0800

    update(astrocore.lua): enable astrocore config
    - comment out the switch;

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -1,4 +1,5 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- 注释掉下一行以激活配置
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCore provides a central place to modify mappings, vim options, autocommands, and more!
 -- Configuration documentation can be found with `:h astrocore`
