d176654c612b85dc3a57626e602a4e7726ae9124
doraemon-hub-art <1660219734@qq.com>
2026-03-13 15:39:40 +0800

    feat(themes.lua): add theme manager
    - set default theme is astrodark;

---
+++ lua/plugins/themes.lua
@@ -0,0 +1,8 @@
+return {
+  -- Nightfox 系列（包含 Carbonfox）
+  { "EdenEast/nightfox.nvim", lazy = false, priority = 1000 },
+  -- TokyoNight 系列
+  { "folke/tokyonight.nvim", lazy = false, priority = 1000 },
+  -- Kanagawa
+  { "rebelot/kanagawa.nvim", lazy = false, priority = 1000 },
+}
