93033085a9e2fc53a908752b27dbe70795910289
doraemon-hub-art <1660219734@qq.com>
2026-05-13 17:29:56 +0800

    update(avante): modify api key name

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -12,7 +12,7 @@ return {
           __inherited_from = "openai", -- 必须有这一行，否则它不知道怎么发请求
           endpoint = "https://api.deepseek.com",
           model = "deepseek-v4-flash", -- 注意：目前官方是 deepseek-chat 或 deepseek-reasoner，没有 v4-flash
-          api_key_name = "OPENAI_API_KEY", -- 建议写变量名，然后在系统里设置
+          api_key_name = "DEEPSEEK_API_KEY", -- 建议写变量名，然后在系统里设置 
           timeout = 30000,
           temperature = 0,
           max_tokens = 4096,
