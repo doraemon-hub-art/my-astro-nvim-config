cad9de693896d91271c0bc64d1dc7eaca3784562
doraemon-hub-art <1660219734@qq.com>
2026-05-14 11:08:24 +0800

    feat(community): add more useful float terimal
    - todo: occasional shortcut key conflicts;

---
+++ lua/plugins/floaterm.lua
@@ -0,0 +1,29 @@
+return {
+  "nvzone/floaterm",
+  dependencies = {
+    { "nvzone/volt", lazy = true },
+    {
+      "AstroNvim/astrocore",
+      opts = {
+        mappings = {
+          -- Normal 模式映射
+          n = {
+            ["<C-t>"] = { "<cmd>FloatermToggle<cr>", desc = "Toggle Floaterm" },
+            ["<leader>fn"] = { "<cmd>FloatermNew<cr>", desc = "New Floaterm" },
+            ["<leader>fj"] = { "<cmd>FloatermNext<cr>", desc = "Next Floaterm" },
+            ["<leader>fk"] = { "<cmd>FloatermPrev<cr>", desc = "Previous Floaterm" },
+          },
+          -- Terminal 模式映射 (确保你在终端里也能随时用 Ctrl+t 关掉)
+          t = {
+            ["<C-t>"] = { "<cmd>FloatermToggle<cr>", desc = "Toggle Floaterm" },
+          },
+        },
+      },
+    },
+  },
+  cmd = { "FloatermToggle", "FloatermNew", "FloatermNext", "FloatermPrev" },
+  opts = {
+    border = true,
+    size = { h = 70, w = 90 },
+  },
+}
